a5dc11182217cb5a07b63b770cce4edc45e1b7fc
ashley <ashley.addison1990@gmail.com>
2018-05-18 16:48:54 -0700

    Delete server.js

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => console.log('Hello World welcome to my app!'))
-
- const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
